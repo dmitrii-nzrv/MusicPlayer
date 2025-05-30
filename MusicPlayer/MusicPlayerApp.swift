66b2ec62b01e976a396e9069e852cadc8c0b6681
Dmitrii N <letsgetstarted.dmitrii@gmail.com>
2025-05-30 20:07:05 +0300

    first commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Dmitrii Nazarov on 30.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
